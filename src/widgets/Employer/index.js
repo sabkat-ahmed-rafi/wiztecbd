cf53385bad3130c42b7fa88da7a15bfbf92ba9d7
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2026-03-04 12:38:35 +0600

    feat: integrate api

--- src/widgets/Employer/index.js
+++ src/widgets/Employer/index.js
@@ -1,18 +1,51 @@
-import React from "react";
+'use client'
+
+import React, { useEffect, useState } from "react";
 
 import EmplCard from "@/components/EmpCard";
 import ScrollAnimatedSection from "@/components/ScrollAnimationSection";
-import { teamData } from "@/app/staticData/teams";
+import api from "@/config/api";
 
 const Employer = () => {
+    const [employees, setEmployees] = useState([]);
+
+    useEffect(() => {
+        const fetchEmployees = async () => {
+            try {
+                const response = await api.get("/api/get-employees");
+                console.log(response)
+                if (response.data.status === 200 || response.data.status === 201) {
+                    setEmployees(response.data.employees);
+                }
+            } catch (error) {
+                console.error("Failed to fetch employees:", error);
+            }
+        };
+
+        fetchEmployees();
+    }, []);
+
     return (
         <div>
             <ScrollAnimatedSection>
-                <h1 className="text-H1 text-center text-primary font-bold mb-12">Meet The Team</h1>
+                <h1 className="text-center text-primary font-bold mb-12 md:text-H1 text-H2 leading-tight">Meet The Team</h1>
             </ScrollAnimatedSection>
             <div className=" grid lg:grid-cols-3 grid-cols-2 sm:grid-cols-2 md:gap-4 gap-2">
-                {teamData.map((team) => (
-                    <EmplCard key={team.id} social={team.social} name={team.name} department={team.department} img={team.img} alt={team.alt} description={team.description} />
+                {employees.map((team) => (
+                    <EmplCard
+                        key={team.id}
+                        social={{
+                            facebook: team.facebook || "#",
+                            instagram: team.instagram || "#",
+                            twitter: team.twitter || "#",
+                            linkedin: team.linkedin || "#",
+                        }}
+                        name={team.name}
+                        department={team.designation}
+                        img={team.image}
+                        alt={team.name}
+                        description={team.details}
+                    />
                 ))}
             </div>
         </div>
