c944a63fafa81bd1d9a5062a0e9f353c7a13ea55
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2026-04-05 14:58:28 +0600

    chore: change the base url

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `http://${process.env.NEXT_PUBLIC_API_BASE_URL || process.env.VITE_API_BASE_URL}`,
+  baseURL: `${process.env.NEXT_PUBLIC_API_BASE_URL || process.env.VITE_API_BASE_URL}`,
   headers: {
     "x-api-key": process.env.NEXT_PUBLIC_API_KEY || process.env.VITE_API_KEY
   },
